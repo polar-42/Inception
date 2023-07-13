dee89b4c5fd906f80b791de5fa053f20b99c1169
vm_42 <letolguenec.felix44@gmail.com>
2023-07-13 16:09:12 +0200

    save

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+all:
+	docker-compose -f srcs/docker-compose.yml up -d
+
+stop:
+	docker stop mariadb wordpress nginx
+
 clean:
-	docker rm $(docker ps -aq)
+#	docker rm $(docker ps -aq)
+	docker system prune -af
 
---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rc-service mariadb start
+
+mariadb-secure-installation << EOF
+Inception42
+n
+y
+Inception42
+Inception42
+y
+y
+y
+y
+EOF
+
+echo "MariaDB finish" > test
+cat test
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+#mysqladmin -u root -p$SQL_ROOT_PASSWORD #shutdown
+
+#exec mysqld_safe
