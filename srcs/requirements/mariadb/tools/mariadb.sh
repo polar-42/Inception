82371c7013298a11950c2e26a8156602f31b2c8d
vm_42 <letolguenec.felix44@gmail.com>
2023-07-21 09:56:50 +0200

    almost finish

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -22,7 +22,7 @@ else
     mkdir -p /var/tmp
 
     /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 \
-    --skip-networking=0 << EOF
+    --skip-networking=0 > /dev/null 2>&1 << EOF
     USE mysql;
     FLUSH PRIVILEGES ;
     CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
@@ -33,45 +33,10 @@ else
     SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${SQL_ROOT_PASSWORD}') ;
 EOF
 fi
-    # mkdir /run/openrc
-    # touch /run/openrc/softlevel
-    # rc-service mariadb setup
-    # rc-update add mariadb default
-    # rc-status
-    # rc-service mariadb start
-
-    # mariadb-secure-installation << EOF
-    
-    # n
-    # y
-    # ${SQL_PASSWORD}
-    # ${SQL_PASSWORD}
-    # y
-    # y
-    # y
-    # y
-# EOF
-
-# mysql -uroot -p$SQL_PASSWORD << EOF
-# CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
-# CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
-# GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-# FLUSH PRIVILEGES;
-# EOF
-#     #mysqladmin -u root -p$SQL_ROOT_PASSWORD #shutdown
-
-#     #exec mysqld_safe
-
-#     #echo SUCESS
-# fi
-
-# mysqld_safe -uroot -p$SQL_PASSWORD --skip-grant-tables
 
 if [ -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
     echo ${SQL_DATABASE} is created
 fi
 
 exec /usr/bin/mysqld --user=mysql --skip-name-resolve \
-    --skip-networking=0 2> /dev/null
-    
+    --skip-networking=0 2> /dev/null 2> /dev/null 
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,34 +1,13 @@
 #!/bin/sh
 
-#sleep 10
-
-
-# wp config create --path='/var/www/wordpress' \
-#     --dbname=$SQL_DATABASE \
-#     --dbuser=$SQL_USER --allow-root \
-#     --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306
-
-
-# /usr/sbin/php-fpm81
-
-
-# chown -R nginx:www-data /var
-
-# mkdir -p /usr/logs/php-fpm
-# mkdir -p /usr/html
-
-# wget https://wordpress.org/wordpress-6.2.2.tar.gz -P /var/www
-# cd /var/www/ && tar -xzvf wordpress-6.2.2.tar.gz
-
 if [ ! -d "/var/www/wordpress/wp-admin" ]; then
 
-    sleep 10
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -q
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
 
-    wget https://wordpress.org/wordpress-6.2.2.tar.gz -P /var/www
-    cd /var/www/ && tar -xzvf wordpress-6.2.2.tar.gz
+    wget https://wordpress.org/wordpress-6.2.2.tar.gz -q -P /var/www
+    cd /var/www/ && tar -xzf wordpress-6.2.2.tar.gz
 
     sed -i "s/database_name_here/$SQL_DATABASE/g" /var/www/wordpress/wp-config-sample.php
     sed -i "s/username_here/$SQL_USER/g" /var/www/wordpress/wp-config-sample.php
@@ -38,9 +17,13 @@ if [ ! -d "/var/www/wordpress/wp-admin" ]; then
     cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
 
     cd /var/www/wordpress && wp core install --allow-root --url=fle-tolg.42.fr \
-        --title=FleTolgInception \
+        --title=Inception \
         --admin_user=$SQL_USER --admin_password=$SQL_PASSWORD \
         --admin_email=fle-tolg@student.42angouleme.fr --skip-email
+else
+    echo "Wordpress already installed"
 fi
+
+echo "Wordpress is ready to use"
 
 php-fpm81 -F -R
